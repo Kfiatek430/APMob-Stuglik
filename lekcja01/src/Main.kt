d71f61ca473dd3b7caba30c6db157fb5024f73d4
Michał Kwiatkowski <mkwiatkowski@ppinf.local>
2025-09-04 09:05:39 +0200

    lekcja01

---
+++ lekcja01/src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val name = "Kotlin"
+
+    println("Hello, $name!")
+
+    for (i in 1..5) {
+        println("i = $i")
+    }
+
+    for (j in 5 downTo 1) {
+        println("j = $j")
+    }
+}
