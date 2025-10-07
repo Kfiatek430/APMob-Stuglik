8319559e037d828a2cb1ed857d367574551f3e18
Michał Kwiatkowski <mkwiatkowski@ppinf.local>
2025-10-07 09:07:37 +0200

    first-project

---
+++ first-project/app/src/main/java/com/example/first_project/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.first_project
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            Toast.makeText(this, "Hello world", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
